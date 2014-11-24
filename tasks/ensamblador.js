cff1a33563e193d7216832cb035e04c532c66613
Leonel Dan Fishkel <lfishkel@truelgicsoftware.com>
2014-11-24 15:12:46 -0300

    primer commit de automatizacion

---
+++ tasks/ensamblador.js
@@ -0,0 +1,28 @@
+var	spawn = require("child_process").spawn,
+    fs = require("fs");
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("shell:ensamblador", "Ensamblador clone, make, ensamblar", function() {
+
+        var done = this.async(),
+            cmd = "git clone";
+            args = ["https://github.com/sisoputnfrba/so-ensamblador"];
+
+        var clone = spawn(cmd, args);
+
+        clone.stdout.on("data", function(chunk) {
+            grunt.log.write(chunk);
+		});
+
+        clone.stderr.on("data", function(chunk) {
+            grunt.log.error(chunk);
+		});
+
+        clone.on("close", function(code) {
+            done();
+        });
+
+    });
+
+}
